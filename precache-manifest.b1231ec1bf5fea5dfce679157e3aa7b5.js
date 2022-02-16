6a091b78503c81acfc2716b2e446e7777ccef7f1
Kousar <kousarbanua1999@gmail.com>
2022-02-16 17:27:18 +0530

    Updates

---
+++ precache-manifest.b1231ec1bf5fea5dfce679157e3aa7b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172aa4eb83848b24559b8120c8b7d696",
+    "url": "/React-JS/index.html"
+  },
+  {
+    "revision": "a9ad00896a35c23f4852",
+    "url": "/React-JS/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97d9f75bd849666f693e",
+    "url": "/React-JS/static/js/2.2bd88f43.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "/React-JS/static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "a9ad00896a35c23f4852",
+    "url": "/React-JS/static/js/main.3e36e771.chunk.js"
+  },
+  {
+    "revision": "32191d066c78ecdf4550",
+    "url": "/React-JS/static/js/runtime~main.c85364c9.js"
+  }
+]);
